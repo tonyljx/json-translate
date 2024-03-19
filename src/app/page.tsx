1fbf3ff828ccf3597291eb72957c39d37fe13ac8
liangjiongxin <liangjiongxin@gmail.com>
2024-03-20 04:08:12 +0800

    feat: update config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
             priority
           />
           <h1 className="text-2xl font-semibold tracking-tight">
-            Json Translator
+            Json Translate
           </h1>
         </Link>
 
